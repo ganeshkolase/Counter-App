9aeb555047d08f4117c48868c9a430dcce350d5d
Ganesh <ganesh.kolase@techverito.com>
2021-02-12 19:38:14 +0530

    Add counter functioalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+  count: number;
+  minDecreaseLimit = 0;
+  maxIncreaseLimit = 10;
+  alert = false;
+
+  constructor() {
+    this.count = 0;
+  }
+
+  counterIncrease(): void {
+    if (this.count < this.maxIncreaseLimit) {
+      this.count += 1;
+      this.discardAlert();
+    } else {
+      this.showAlert();
+    }
+  }
+
+  private showAlert(): void {
+    this.alert = true;
+  }
+
+  discardAlert(): void {
+    this.alert = false;
+  }
+
+  counterDecrease(): void {
+    if (this.count !== this.minDecreaseLimit) {
+      this.count -= 1;
+      this.discardAlert();
+    } else {
+      this.showAlert();
+    }
+  }
+
+  counterReset(): void {
+    this.count = 0;
+    this.discardAlert();
+  }
 }
